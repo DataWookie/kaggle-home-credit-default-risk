b126945d10c4d15537de3eb601d4bcc6b585a274
Andrew B. Collier <andrew@exegetic.biz>
2018-06-08 13:07:04 +0200

    Timestamp files.

--- 3-submit.R
+++ 3-submit.R
@@ -1,8 +1,18 @@
-submission = cbind(SK_ID_CURR = data_test$sk_id_curr, TARGET = predict(fit, test, type = "response")) %>%
+TIME <- format(Sys.time(), "%Y%m%d-%H%M%S")
+
+sink(sprintf("%s.txt", TIME))
+print(fit)
+cat("\n")
+fit$results %>% arrange(desc(ROC))
+sink()
+
+saveRDS(fit, file = sprintf("%s.rds", TIME))
+
+submission = cbind(SK_ID_CURR = data_test$sk_id_curr, TARGET = predict(fit, test, type = "prob")) %>%
   data.frame() %>%
   #
   # This is necessary to ensure that output not converted to scientific notation.
   #
   mutate(SK_ID_CURR = as.integer(SK_ID_CURR))
 
-write.csv(submission, "test-submission.csv", quote = FALSE, row.names = FALSE)
+write.csv(submission, sprintf("%s.csv", TIME), quote = FALSE, row.names = FALSE)
