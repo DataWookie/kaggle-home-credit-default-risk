1e872daf4362e99ba2105162fccdd251ff86f86a
Andrew B. Collier <andrew@exegetic.biz>
2018-06-10 06:12:20 +0200

    Removed old code for imputation.

--- home-credit-default-risk.R
+++ home-credit-default-risk.R
@@ -65,65 +65,13 @@ data <- data %>% mutate(
 
 # FILL MISSING --------------------------------------------------------------------------------------------------------
 
-# Impute median value using only values for training data.
-#
-impute_median <- function(col) {
-  median_train <- data %>% filter(set == "train") %>% pull(col) %>% median(na.rm = TRUE)
-  #
-  ifelse(is.na(data[, col]), median_train, data[, col])
-}
-
 missing_to_zero <- function(values) {
   ifelse(is.na(values), 0, values)
 }
 
+# TODO: These might better be replaced with mean/median.
+#
 data <- data %>% mutate(
-  # amt_annuity = impute_median("amt_annuity"),
-  # amt_goods_price = impute_median("amt_goods_price"),
-  # cnt_fam_members = impute_median("cnt_fam_members"),
-  # commonarea_medi = impute_median("commonarea_medi"),
-  # commonarea_mode = impute_median("commonarea_mode"),
-  # elevators_medi = impute_median("elevators_medi"),
-  # elevators_mode = impute_median("elevators_mode"),
-  # entrances_mode = impute_median("entrances_mode"),
-  # entrances_medi = impute_median("entrances_medi"),
-  # floorsmax_mode = impute_median("floorsmax_mode"),
-  # floorsmax_medi = impute_median("floorsmax_medi"),
-  # floorsmin_mode = impute_median("floorsmin_mode"),
-  # floorsmin_medi = impute_median("floorsmin_medi"),
-  # landarea_mode = impute_median("landarea_mode"),
-  # landarea_medi = impute_median("landarea_medi"),
-  # livingarea_mode = impute_median("livingarea_mode"),
-  # livingarea_medi = impute_median("livingarea_medi"),
-  # nonlivingarea_mode = impute_median("nonlivingarea_mode"),
-  # nonlivingarea_medi = impute_median("nonlivingarea_medi"),
-  # livingapartments_mode = impute_median("livingapartments_mode"),
-  # livingapartments_medi = impute_median("livingapartments_medi"),
-  # nonlivingapartments_mode = impute_median("nonlivingapartments_mode"),
-  # nonlivingapartments_medi = impute_median("nonlivingapartments_medi"),
-  # obs_30_cnt_social_circle = impute_median("obs_30_cnt_social_circle"),
-  # def_30_cnt_social_circle = impute_median("def_30_cnt_social_circle"),
-  # obs_60_cnt_social_circle = impute_median("obs_60_cnt_social_circle"),
-  # def_60_cnt_social_circle = impute_median("def_60_cnt_social_circle"),
-  # amt_req_credit_bureau_hour = impute_median("amt_req_credit_bureau_hour"),
-  # amt_req_credit_bureau_day = impute_median("amt_req_credit_bureau_day"),
-  # amt_req_credit_bureau_week = impute_median("amt_req_credit_bureau_week"),
-  # amt_req_credit_bureau_mon = impute_median("amt_req_credit_bureau_mon"),
-  # amt_req_credit_bureau_qrt = impute_median("amt_req_credit_bureau_qrt"),
-  # amt_req_credit_bureau_year = impute_median("amt_req_credit_bureau_year"),
-  # apartments_mode = impute_median("apartments_mode"),
-  # apartments_medi = impute_median("apartments_medi"),
-  # basementarea_mode = impute_median("basementarea_mode"),
-  # basementarea_medi = impute_median("basementarea_medi"),
-  # totalarea_mode = impute_median("totalarea_mode"),
-  # years_beginexpluatation_mode = impute_median("years_beginexpluatation_mode"),
-  # years_beginexpluatation_medi = impute_median("years_beginexpluatation_medi"),
-  # years_build_mode = impute_median("years_build_mode"),
-  # years_build_medi = impute_median("years_build_medi"),
-  # days_last_phone_change = impute_median("days_last_phone_change"),
-  #
-  # TODO: These might better be replaced with mean/median.
-  #
   own_car_age = missing_to_zero(own_car_age),
   ext_source_1 = missing_to_zero(ext_source_1),
   ext_source_2 = missing_to_zero(ext_source_2),
