406eb1c180d2cdb5ef9d3caadd6609985d4c5cdc
Andrew B. Collier <andrew@exegetic.biz>
2018-06-10 07:59:39 +0200

    Method from environment variable.

--- run-jobs.sh
+++ run-jobs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for model in glm rpart xgboost svm
+for model in glm rpart xgbTree svmRadial
 do
-  cat 0-load-data.R  1-prepare.R  2-train-${model}.R  3-submit.R | R --slave >log-${model}.txt &
+  METHOD=$model bash -c "cat home-credit-default-risk.R | R --slave >log-${model}.txt"
 done
