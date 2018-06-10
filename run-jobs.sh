2b49e0813c86bfb7445dea14cd266d0ef87547f2
Andrew B. Collier <andrew@exegetic.biz>
2018-06-10 11:33:34 +0200

    Redirect stderr to log file.

--- run-jobs.sh
+++ run-jobs.sh
@@ -2,5 +2,5 @@
 
 for model in glm rpart svmRadial xgbTree
 do
-  DEBUG=FALSE METHOD=$model bash -c "cat home-credit-default-risk.R | R --slave >log-${model}.txt" &
+  DEBUG=FALSE METHOD=$model bash -c "cat home-credit-default-risk.R | R --slave >log-${model}.txt 2>&1" &
 done
